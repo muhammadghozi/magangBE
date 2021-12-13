b4479d3cad6d29ef7fc9d4094cd9ba02731ba478
muhammad ghozi alfattah <muhammadghozi@upi.edu>
2021-12-13 16:31:58 +0700

    menambahkan fastift static dan nodemon

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const path = require('path')
 
 //register plugin register
 fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
+  root: path.join(__dirname, "public"),
   prefix: '/', // optional: default '/'
 });
 
 // Declare a route
-fastify.get("/", async (request, reply) => {
+fastify.get("/public/", async (request, reply) => {
   reply.sendFile ("index.html"); // serving path.join(__dirname, 'public', 'index.html') directly
 });
 
