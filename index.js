f2962bc133df8d6a49c6fe47a515f63e16f3031e
muhammad ghozi alfattah <muhammadghozi@upi.edu>
2021-12-14 18:46:29 +0700

    gagfgh

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ fastify.get("/apaya", async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(5000);
+    await fastify.listen(process.env.PORT,"0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
