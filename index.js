509ba2b1662f90b35f99be915a7082d821d9391f
muhammad ghozi alfattah <muhammadghozi@upi.edu>
2021-12-15 15:43:09 +0700

    gsgshh

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true });
-const path = require('path');
+const fastify = require('fastify')({ logger: true })
+const path = require('path')
 
-//register plugin registerh
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, "../public"),
-  prefix: '/', // optional: default '/'
-  decorateReplay: false,
-});
-
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, "../public/assets"),
-  prefix: '/assets', // optional: default '/'
-  decorateReplay: false,
-});
-
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, "../public/forms"),
-  prefix: '/forms', // optional: default '/'
-  decorateReplay: false,
-});
+// Register Plugin
+fastify.register(require('fastify-static'), require ("./config/static").public)
 
 // Declare a route
-fastify.get("/", async (request, reply) => {
-  reply.sendFile ("index.html"); // serving path.join(__dirname, 'public', 'index.html') directly process.env.PORT,"0.0.0.0"
-});
-
-// Declare a route
-fastify.get("../apaya", async (request, reply) => {
-  return { hello: 'apaya' };
-});
+fastify.get('/', function (req, reply) {
+  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT,"0.0.0.0");  
+    await fastify.listen(process.env.PORT, "0.0.0.0")
   } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-};
-  
+}
+
+start()
 start();
